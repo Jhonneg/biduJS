9c5aaff959a7c5fb0aa90fa8cf82dd81da2e7b71
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-05-31 22:41:17 -0300

    basic state management implemented

--- bidu.js
+++ bidu.js
@@ -1,6 +1,25 @@
 function render(element, content) {
-    const app = document.querySelector(element)
-    if (app !== null) {
-        app.innerHTML = content
-    }
+  const app = document.querySelector(element);
+  if (app !== null) {
+    app.innerHTML = content;
+  }
+}
+function reactive(obj) {
+  const keys = Object.keys(obj);
+  const reactiveObj = {};
+  keys.forEach((key) => {
+    let value = obj[key];
+    Object.defineProperty(reactiveObj, key, {
+      get() {
+        console.log(`Getting value, ${value}`);
+        return value;
+      },
+      set(newValue) {
+        console.log(`Setting value, ${newValue}`);
+        value = newValue;
+        renderApp();
+      },
+    });
+  });
+  return reactiveObj;
 }
