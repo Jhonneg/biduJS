99882901fe5ac05f376bd94834ee34da97fad407
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-06-02 16:31:00 -0300

    added createEffect hook

--- bidu.js
+++ bidu.js
@@ -1,3 +1,6 @@
+const depsMap = Map();
+let currentEffect = null;
+
 function render(element, content) {
   const app = document.querySelector(element);
   if (app !== null) {
@@ -12,14 +15,56 @@ function reactive(obj) {
     Object.defineProperty(reactiveObj, key, {
       get() {
         console.log(`Getting value, ${value}`);
+        track(reactiveObj, key);
         return value;
       },
       set(newValue) {
         console.log(`Setting value, ${newValue}`);
-        value = newValue;
-        renderApp();
+        if (newValue !== value) {
+          value = newValue;
+          trigger(reactiveObj, key);
+          renderApp();
+        }
       },
     });
   });
+  function track(target, key) {
+    if (currentEffect) {
+      let deps = depsMap.get(target);
+      if (!deps) {
+        deps = new Map();
+        deps.set(target, deps);
+      }
+      dep = deps.get(key);
+      if (!dep) {
+        dep = new Set();
+        deps.set(key, dep);
+      }
+      deps.add(createEffect);
+    }
+  }
+  function createEffect(fn) {
+    const effect = function effect(...args) {
+      try {
+        currentEffect = effect;
+        return fn(...args);
+      } finally {
+        currentEffect = null;
+      }
+    };
+    effect();
+  }
+
+  function trigger(target, key) {
+    const deps = depsMap.get(target);
+    if (!deps) return;
+    const dep = deps.get(key);
+    if (dep) {
+      const effectsToRun = new Set(dep);
+      effectsToRun.forEach((effect) => {
+        effect();
+      });
+    }
+  }
   return reactiveObj;
 }
